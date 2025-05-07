e14fa5dd741fae8b4a49470a8c49eee4990f6d26
Ottowski <otto.arvidsson94@gmail.com>
2025-05-07 21:58:33 +0200

    added src componets to main class

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from src.data_loader import load_weather_data
+from src.data_model import train_linear_regression
+from src.data_processor import data_processor
 
 def main():
     # Reads data
     WH = load_weather_data("data/weatherHistory.csv")
+    
+    # Treat data
+    WH_clean = data_processor(WH)
+    
+    # Trains data
+    Model = train_linear_regression(WH_clean)
 
     print("\n Modell is trained and ready for use!")
 
